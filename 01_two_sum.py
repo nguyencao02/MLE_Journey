90cce79a7243fce24864ada489b05f4f2980664c
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-10 16:30:57 -0700

    solve: 1 two sum using python dictionary

---
+++ 01_two_sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int)->List[int]:
+        my_dict={}
+        for y,x in enumerate(nums):
+            n = target - x
+            if n in my_dict:
+                return [my_dict(n),y]
+            else:
+                my_dict[x] = y
