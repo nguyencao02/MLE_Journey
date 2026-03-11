5003926b4c46abcc0d30589e4ad46a8dbd22e54a
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-10 21:40:18 -0700

    solve majority element problem

---
+++ 05_majority_element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        my_dict ={}
+        for ele, x in enumerate(nums):
+            if x in my_dict:
+                my_dict[x] += 1
+            else:
+                my_dict[x] = 1
+            if my_dict[x] > len(nums)/2:
+                return x
