2bfd7b97648ef114b9b9b38c7be15946972ce0ca
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-14 14:46:46 -0700

    Solve: ransom note

---
+++ 17_ransom_note.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c) :
+                return False
+        return True
+                
