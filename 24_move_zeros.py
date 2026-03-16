05e85bdc51168ed4bfa909a2545232bf4ad0f5aa
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-16 10:24:04 -0700

    Solve: Move Zeors

--- 24_move_zeros.py
+++ 24_move_zeros.py
@@ -3,4 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
+        slow = 0 
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow],nums[fast] = nums[fast], nums[slow]
+                slow += 1
