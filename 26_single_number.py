e8d341192dddea36f3b021e34417739f87a4d43b
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-16 10:52:13 -0700

    Solve: single number

---
+++ 26_single_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        count_map = {}
+        for n in nums:
+            if n in count_map:
+                count_map[n] += 1
+            else:
+                count_map[n] = 1
+        for n in nums:
+            if count_map[n] == 1:
+                return n
