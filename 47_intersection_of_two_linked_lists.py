bdbc530a3339e54867d21ff9475318bb841a705a
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-19 12:55:44 -0700

    Solve: intersection of two linked list

---
+++ 47_intersection_of_two_linked_lists.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode)->Optional[ListNode]:
+        pA = headA
+        pB = headB
+        while pA != pB:
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+        return pA
