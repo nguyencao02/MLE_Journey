36129d0b62280c1c7b7c9bf818f358a4f84e13f5
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-21 21:23:17 -0700

    Solve: greatest common divisor of strings

---
+++ 54_greatest_common_divisor_of_strings.py
@@ -0,0 +1,7 @@
+import math
+class Soluton:
+    def gcdOfStrings(self, str1 : str, str2 :str)-> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        length_gcd = math.gcd(len(str1),len(str2))
+        return str1[:length_gcd]
