fc14b5d0fcea0a1ced8796412373c3fcb3fda2b3
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-23 12:54:43 -0700

    Solve: Reserve linked list

---
+++ 62_reserve_linked_list.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reserveList(self, head: Optional[ListNode])-> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
+        
