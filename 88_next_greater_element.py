c5563945973d81c053f052c3629cd168ad0a0065
MyCao <caonguyenyenmy2408@gmail.com>
2026-03-30 13:09:23 -0700

    Solve: next greater element I

---
+++ 88_next_greater_element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int])-> List[int]:
+        greater_map = {}
+        stack = []
+        for num in nums2:
+            while stack and num > stack[-1]:
+                smaller_num = stack.pop()
+                greater_map[smaller_num] = num
+            stack.append(num)
+        res = []
+        for n in res:
+            res.append(greater_map(n,-1))
+        
