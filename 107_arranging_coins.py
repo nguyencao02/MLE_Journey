5a48925aa974d57c2c422b94c28691d21e868475
MyCao <caonguyenyenmy2408@gmail.com>
2026-04-01 15:53:59 -0700

    Solve: arranging coins

--- 107_arranging_coins.py
+++ 107_arranging_coins.py
@@ -1,3 +1,14 @@
 class Solution:
     def arrangeCoins(self, n: int)-> int:
-       return 
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            total = mid * (mid + 1) // 2
+            if total == n:
+                return mid
+            elif total < n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
