41af54a1cbf7478091a3abd3d602dde8a2315863
MyCao <caonguyenyenmy2408@gmail.com>
2026-04-01 15:49:04 -0700

    Solve: sqrt(X)

--- 111_srqt(x).py
+++ 111_srqt(x).py
@@ -1,3 +1,14 @@
 class Solution:
     def mySqrt(self, x: int)-> int:
-        return
+        left = 1
+        right = x
+        while left <= right:
+            mid = left + (right - left) // 2
+            sqrt = mid * mid
+            if sqrt == x:
+                return mid
+            elif sqrt < x:
+                left = mid + 1
+            else:
+                right = mid - 1                
+        return right
