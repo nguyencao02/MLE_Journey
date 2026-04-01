1660e0b562506b67fdba4dfd8536d7c79fd12502
MyCao <caonguyenyenmy2408@gmail.com>
2026-04-01 16:13:46 -0700

    Solve: valid perfect square

--- 110_valid_perfect_square.py
+++ 110_valid_perfect_square.py
@@ -1,3 +1,14 @@
 class Solution:
     def isPerfectSquare(self, num: int)-> bool:
-        return
+        left = 0
+        right = num
+        while left <= right:
+            mid = left + (right - left) // 2
+            sqr = mid * mid
+            if sqr == num:
+                return True
+            elif sqr < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
