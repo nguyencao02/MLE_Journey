66f0f74008f1b9b67efb5ebbca2b3780e370d809
MyCao <caonguyenyenmy2408@gmail.com>
2026-04-01 16:04:36 -0700

    Solve: guess number higher or lower

--- 109_guess_number_higher_or_lower.py
+++ 109_guess_number_higher_or_lower.py
@@ -1,3 +1,13 @@
 class Solution:
     def guessNumber(self, n: int)-> int:
-        return
+        left = 1
+        right = n
+        while left <= right:
+            mid = left + (right - left) // 2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res == 1:
+                left = mid + 1
+            elif res == -1:
+                right = mid - 1
